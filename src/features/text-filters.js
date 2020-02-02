297af51f4ce601880571869f8614516062335be6
its <its@luzer.sk>
2020-02-02 01:13:28 +0100

    Added missing number rounding filter.

--- src/features/text-filters.js
+++ src/features/text-filters.js
@@ -3,7 +3,12 @@ import Vue from 'vue'
 export default class TextFilters
 {
 	register() {
+		Vue.filter('round', this.round)
 		Vue.filter('shortClass', this.shortClass)
+	}
+
+	round(input, precision = 0) {
+		return Number.parseFloat(input).toFixed(precision)
 	}
 
 	shortClass(className) {
