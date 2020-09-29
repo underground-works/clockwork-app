1c6e1d4942df1089babc66602cee668ad700dd38
its <its@luzer.sk>
2020-09-29 20:34:14 +0200

    Removed unnecesssary vue-clipboard setting.

--- src/vendor.js
+++ src/vendor.js
@@ -10,8 +10,6 @@ Vue.config.devtools = true
 Vue.config.performance = true
 Vue.config.productionTip = false
 
-VueClipboard.config.autoSetContainer = true
-
 Vue.use(VueClipboard)
 Vue.use(VueMoment)
 Vue.use(vClickOutside)
