38ba7e174eea87167046e0e98db6d0dacaef5fe5
its <its@luzer.sk>
2023-06-25 22:24:20 +0200

    Fix Vite build dir setting.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
   plugins: [vue(), svgLoader()],
   resolve: {
     extensions: ['.mjs', '.js', '.vue']
