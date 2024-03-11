7ea76110cd78da0a314f8224f5eaf5818b1516a4
its <its@luzer.sk>
2024-03-11 22:58:26 +0100

    Fixed build using absolute paths.

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,15 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   build: {
     outDir: 'build'
   },
   plugins: [vue(), svgLoader()],
   resolve: {
     extensions: ['.mjs', '.js', '.vue']
+  },
+  experimental: {
+    renderBuiltUrl: filename => filename
   }
 })
