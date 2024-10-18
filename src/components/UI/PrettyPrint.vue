fa364c992e6e16ee8d5e4e1a1b0c2585f9d2a3c6
its <its@luzer.sk>
2024-10-19 01:12:59 +0200

    Pretty-printed text is now shortened by default if it's over 1000 characters long.

--- src/components/UI/PrettyPrint.vue
+++ src/components/UI/PrettyPrint.vue
@@ -1,5 +1,6 @@
 <template>
-	<div></div>
+	<div ref="content"></div>
+	<a v-if="hasLongText && ! showFullText" @click.prevent="showFullText = true" class="pretty-print-more-link" href="#">Show more</a>
 </template>
 
 <script>
@@ -10,6 +11,10 @@ import linkify from 'linkify-html'
 export default {
 	name: 'PrettyPrint',
 	props: [ 'data', 'expanded', 'linkify' ],
+	data: () => ({
+		hasLongText: false,
+		showFullText: false
+	}),
 	methods: {
 		render() {
 			let data = this.data
@@ -29,25 +34,47 @@ export default {
 				try {
 					rendered.append((new PrettyJason(data)).generateHtml())
 				} catch (e) {
-					rendered.innerText = data
+					this.hasLongText = data.length > 1000
+
+					rendered.innerText = this.hasLongText && ! this.showFullText
+						? data.substring(0, 1000) + '...' : data
+
 					if (this.linkify) rendered.innerHTML = linkify(rendered.innerHTML, {
 						target: (href, type) => type === "url" && "_blank"
 					})
 				}
 			}
 
-			if (this.$el.firstChild) this.$el.firstChild.remove()
+			if (this.$refs.content.firstChild) this.$refs.content.firstChild.remove()
 
-			this.$el.append(rendered)
+			this.$refs.content.append(rendered)
 
-			if (this.expanded) this.$el.querySelector('.pretty-jason > li > span').click()
+			if (this.expanded) this.$refs.content.querySelector('.pretty-jason > li > span').click()
 		}
 	},
 	mounted() {
 		this.render()
 	},
 	watch: {
-		data() { this.render() }
+		data() { this.render() },
+		showFullText() { this.render() }
 	}
 }
 </script>
+
+<style lang="scss">
+@import '../../mixins.scss';
+
+.pretty-print-more-link {
+	color: #aaa;
+	display: inline-block;
+	flex: 0;
+	font-size: 90%;
+	margin-top: 3px;
+	text-decoration: none;
+
+	@include dark {
+		color: #777;
+	}
+}
+</style>
