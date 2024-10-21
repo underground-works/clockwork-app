f52888472815100ef90f39cf5d56ba9d59bb483b
its <its@luzer.sk>
2024-10-21 21:20:34 +0200

    Build development builds with inline source-maps.

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
   base: '',
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    sourcemap: mode == 'development' ? 'inline' : false
   },
   plugins: [vue(), svgLoader()],
   resolve: {
@@ -22,4 +23,4 @@ export default defineConfig({
       }
     }
   }
-})
+}))
